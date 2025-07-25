db6061d0613b2f0e2eca955b3f28727ca8baaa67
diogogoncalves <dmgdark95@gmail.com>
2025-07-25 03:27:39 +0100

    Update

---
+++ app/Notifications/GenericNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class GenericNotification extends Notification
+{
+    use Queueable;
+
+    protected $title;
+    protected $message;
+
+    public function __construct($title, $message)
+    {
+        $this->title = $title;
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail', 'database'];
+    }
+
+    public function toDatabase(object $notifiable)
+    {
+        return [
+            'title' => $this->title,
+            'message' => $this->message
+        ];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject($this->title)
+            ->line($this->message);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
